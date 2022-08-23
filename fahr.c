43e58d237c0b0d6df0b9cbabc57b28b169f7cdb0
Kanishk-K-U <kanishkkanagaraj.u@gmail.com>
2022-08-23 22:20:04 +0530

    23/08/22

---
+++ fahr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int f,c;
+    int lower, upper, step;
+    lower = 0;
+    upper = 300;
+    step = 20;
+    f = lower;
+    while(f<=upper)
+    {
+        c = 5*(f-32)/9;
+        printf("%d\t%d\t\n", f, c);
+        f = f + step;
+    }
+}
